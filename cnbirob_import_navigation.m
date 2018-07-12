9fba8c6cc32e4b173b839c841b0e9ff216552d72
Luca Tonin <luca.tonin@gmail.com>
2018-07-12 17:43:04 +0200

    Working on bag data importing

--- cnbirob_import_navigation.m
+++ cnbirob_import_navigation.m
@@ -2,8 +2,44 @@ clearvars; clc;
 
 subject = 'aj1';
 
-pattern     = '.online.mi.mi_bhbf';
-extension   = '.bag';
+gdfpattern  = '*.online.mi.mi_bhbf.*.mobile.gdf';
+rospattern  = '*.online.mi.mi_bhbf.*.mobile.bag';
+rosfolder   = 'robot';
+gdffolder   = '';
 experiment  = 'micontinuous';
 datapath    = ['/mnt/data/Research/' experiment '/' subject '_' experiment '/'];
 savedir     = './analysis/';
+
+rosfiles = cnbirob_util_getdata(datapath, rospattern, rosfolder);
+gdffiles = cnbirob_util_getdata(datapath, gdfpattern, gdffolder);
+
+if(length(rosfiles) ~= length(gdffiles))
+    error('chk:nfiles', 'Different number of ros and gdf files');
+end
+nfiles = length(rosfiles);
+
+for fId = 1:nfiles
+    
+    crosfile = rosfiles{fId};
+    cgdffile = gdffiles{fId};
+    
+    util_bdisp(['[io] - Import ' num2str(fId) '/' num2str(nfiles) ' file:']);
+    disp(['  - bag: ' crosfile]);
+    disp(['  - gdf: ' cgdffile]);
+    
+    % Import the bag file
+    bag = rosbag(crosfile);
+    
+    % Import header of the gdf file and get the SampleRate
+    [~, h] = sload(cgdffile);
+    SampleRate = h.SampleRate;
+    
+    % Read events from bag file
+    util_bdisp(['[io] - Reading events from bag']);
+    ros.EVENT = rosbag_event_read(bag, SampleRate);
+    
+    
+end
+
+
+
--- utilities/cnbirob_util_getdata.m
+++ utilities/cnbirob_util_getdata.m
@@ -1,4 +1,21 @@
-function cnbirob_util_getdata(datapath, pattern, subfolder)
+function [files, folders] = cnbirob_util_getdata(datapath, pattern, subfolder)
+% [files, folders] = cnbirob_util_getdata(datapath, pattern [, subfolder])
+%
+% cnbirob_util_getdata returns all the files in the datapath that match the provided pattern.
+% If subfolder argument is provided the function looks for the file in
+% datapath/subfolder.
+% Input:
+%   - datapath  Path where the function starts looking into
+%   - pattern   Pattern of the file to look for (wildcard allowed)
+%   - subfolder [optional] if provided the function looks into datapath/subfolder
+%
+% Output:
+%   - files     Cell array with the found files
+%   - folders   Cell array with the found folders 
+
+    if nargin == 2
+        subfolder = '';
+    end
 
 %% First level directory access
     % Getting all the files in the given datapath
@@ -10,12 +27,39 @@ function cnbirob_util_getdata(datapath, pattern, subfolder)
         cfolder = entries(eId).folder;
         ctype   = entries(eId).isdir;
         if( strcmp(cname, '.') == false && strcmp(cname, '..') == false && ctype == true )
-            folders = cat(1, folders, [cfolder '/' cname '/']);
+            folders = cat(1, folders, fullfile(cfolder, cname));
         end
     end
     nfolders = length(folders);
  
 %% Iterate inside each sub-directory
-   
-    keyboard
+    files = {};
+    for fId = 1:nfolders
+       cfolder = fullfile(folders{fId}, subfolder);
+       
+       % Check if the subfolder exists
+       if(exist(cfolder, 'file') == false)
+           error('chk:sbf', ['Subfolder ' subfolder ' in ' folders{fId} ' does not exist']);
+       end
+
+       centries = dir(fullfile(cfolder, pattern));
+       
+       for eId = 1:length(centries)
+           cname  = centries(eId).name;
+           cpath  = centries(eId).folder;
+           isfold = centries(eId).isdir;
+           
+           if (isfold == true)
+               continue;
+           end
+           
+           files = cat(1, files, fullfile(cpath, cname));
+       end
+
+       if(isempty(files))
+           warning('chk:file', ['No files matching ''' pattern ''' found in ' cfolder '/']);
+       end 
+        
+    end
 end
+
